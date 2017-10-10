f6c6a9c19217deaaed455db20904dda972472084
mrgunter51 <mrgunter@crimson.ua.edu>
2017-10-10 09:33:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function placeOrder(cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.")
   }
   else{
-    console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`)
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
   }
   while(cart.length !== 0){
     cart.pop()
