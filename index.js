e1c3b4adae94f9ba804c8080c5b52ffbf78487d0
mrgunter51 <mrgunter@crimson.ua.edu>
2017-10-10 09:27:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function total() {
 
 function removeFromCart(item) {
   var found = false;
-  
+
   for(let i = 0;i < cart.length;i++){
     if(cart[i].hasOwnProperty(item)){
       cart.splice(i,1)
